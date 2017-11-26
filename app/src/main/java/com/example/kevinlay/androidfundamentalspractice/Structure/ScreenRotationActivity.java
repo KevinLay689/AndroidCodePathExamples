de6218eda4adc05bbc6d69e7cb007fd31661534b
Kevin Lay <kevinlay689@yahoo.com>
2017-11-25 17:57:29 -0800

    Screen rotation Activity now works

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/Structure/ScreenRotationActivity.java
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/Structure/ScreenRotationActivity.java
@@ -3,6 +3,7 @@ package com.example.kevinlay.androidfundamentalspractice.Structure;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,12 +13,14 @@ import com.example.kevinlay.androidfundamentalspractice.R;
 
 public class ScreenRotationActivity extends AppCompatActivity {
 
+    private static final String TAG = "ScreenRotationActivity";
+
     private Button bSave;
     private TextView tvValueHolder;
     private EditText etInput;
 
     private String screenRotationSavedValueKey = "savedValueKey";
-    private String screenRotationSavedValue;
+    private String screenRotationSavedValue = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +31,35 @@ public class ScreenRotationActivity extends AppCompatActivity {
         tvValueHolder = (TextView) findViewById(R.id.tvScreenRotationText);
         etInput = (EditText) findViewById(R.id.etScreenRotation);
 
-        if(savedInstanceState != null) {
-            screenRotationSavedValue = savedInstanceState.getString(screenRotationSavedValueKey);
-        }
-
         bSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 screenRotationSavedValue = etInput.getText().toString();
-                tvValueHolder.setText(screenRotationSavedValue);
+                tvValueHolder.setText(R.string.screen_rotation_textview_saved_text);
+                Log.i(TAG, "onSaveInstanceState: Saved value is:  "+  screenRotationSavedValue );
             }
         });
-
 
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+    protected void onResume() {
+        super.onResume();
+        tvValueHolder.setText(screenRotationSavedValue);
+    }
 
-        super.onSaveInstanceState(outState, outPersistentState);
-        outState.putString(screenRotationSavedValue, screenRotationSavedValueKey);
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        screenRotationSavedValue = savedInstanceState.getString(screenRotationSavedValueKey);
+        tvValueHolder.setText(screenRotationSavedValue);
+        Log.i(TAG, "onRestoreInstanceState: " + screenRotationSavedValue);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(screenRotationSavedValueKey, screenRotationSavedValue);
+        Log.i(TAG, "onSaveInstanceState: " + screenRotationSavedValueKey);
     }
 }
