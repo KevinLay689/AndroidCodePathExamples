334f3c232845a3bcdea94c786d9b9e85b1256416
Kevin Lay <kevinlay689@yahoo.com>
2017-11-25 20:37:41 -0800

    Permissions prototype

---
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/Structure/PermissionsActivity.java
@@ -0,0 +1,15 @@
+package com.example.kevinlay.androidfundamentalspractice.Structure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kevinlay.androidfundamentalspractice.R;
+
+public class PermissionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions);
+    }
+}
