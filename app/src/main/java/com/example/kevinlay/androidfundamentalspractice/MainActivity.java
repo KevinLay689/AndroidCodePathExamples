b2c9fe5f5fef4861f1bf0af732ab5f621da85cf1
Kevin Lay <kevinlay689@yahoo.com>
2017-11-26 12:20:45 -0800

    Basic foundation for views and layouts set up

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/MainActivity.java
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.kevinlay.androidfundamentalspractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.kevinlay.androidfundamentalspractice.Structure.StructureMainActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bEnterStructureActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bEnterStructureActivity = (Button) findViewById(R.id.bEnterStructureActivity);
+        bEnterStructureActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), StructureMainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
