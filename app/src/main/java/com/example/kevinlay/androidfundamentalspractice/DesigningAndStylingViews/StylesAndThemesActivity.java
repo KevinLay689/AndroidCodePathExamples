836732670aef51d1a4b24cf94a88fa8eb085ed95
Kevin Lay <kevinlay689@yahoo.com>
2017-12-03 15:19:57 -0800

    -Added Styles and Themes Activity

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/DesigningAndStylingViews/StylesAndThemesActivity.java
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/DesigningAndStylingViews/StylesAndThemesActivity.java
@@ -5,6 +5,16 @@ import android.os.Bundle;
 
 import com.example.kevinlay.androidfundamentalspractice.R;
 
+/**
+ * Styles and Themes Activity
+ *
+ * Major Takeaways
+ *
+ * -Defined in styles.xml
+ * -Inherit from other styles using parent attribute or use periods like LargeFont.Red.Bold etc
+ * -Themes can be applied at application level or activity level
+ *
+ */
 public class StylesAndThemesActivity extends AppCompatActivity {
 
     @Override
