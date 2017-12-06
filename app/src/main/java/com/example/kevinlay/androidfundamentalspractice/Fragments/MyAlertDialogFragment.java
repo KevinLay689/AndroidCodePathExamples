5c3fd69e69984bdace932ffea0039e5562767709
Kevin Lay <kevinlay689@yahoo.com>
2017-12-06 10:57:18 -0800

    -Added alert dialog fragment

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/Fragments/MyAlertDialogFragment.java
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/Fragments/MyAlertDialogFragment.java
@@ -2,12 +2,18 @@ package com.example.kevinlay.androidfundamentalspractice.Fragments;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
 /**
  * Alert Dialog Fragment
  *
- * <
+ * Major Takeaways
+ * -Needs an empty constructor
+ * -Uses the newInstance static method just like custom dialog
+ * -Uses onCreateDialog to build the fragment instead of inflating a layout, therefor it doesn't need onCreateView
  *
  */
 
@@ -16,8 +22,37 @@ public class MyAlertDialogFragment extends DialogFragment {
 
     }
 
+    public static MyAlertDialogFragment newInstance(String title) {
+        MyAlertDialogFragment fragment = new MyAlertDialogFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("title", title);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+
+        String title = getArguments().getString("title", "No title given");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title);
+        builder.setMessage("Are you sure?");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "You clicked Ok", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        return builder.create();
+
     }
 }
