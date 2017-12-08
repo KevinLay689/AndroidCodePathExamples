ccf8ecbbaa7c98cb64a3d2ade1ff17b7aefef01e
Kevin Lay <kevinlay689@yahoo.com>
2017-12-08 11:29:38 -0800

    -Added Notifications to Services Activity

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/Services/ServicesActivity.java
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/Services/ServicesActivity.java
@@ -1,12 +1,17 @@
 package com.example.kevinlay.androidfundamentalspractice.Services;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,13 +20,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.kevinlay.androidfundamentalspractice.Fragments.ViewPagerActvity;
+import com.example.kevinlay.androidfundamentalspractice.MainActivity;
 import com.example.kevinlay.androidfundamentalspractice.R;
 
 public class ServicesActivity extends AppCompatActivity {
 
     private MyTestReceiver receiverForTest;
 
-    private Button bStartIntentServiceWithResultReceiver, bStartIntentServiceWithBroadcastReceiver, bStartIntentServiceWithAlarmManager;
+    private Button bStartIntentServiceWithResultReceiver, bStartIntentServiceWithBroadcastReceiver, bStartIntentServiceWithAlarmManager,
+                    bBeginNotifications;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +59,84 @@ public class ServicesActivity extends AppCompatActivity {
         bStartIntentServiceWithAlarmManager.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               scheduleAlarm();
+                scheduleAlarm();
             }
         });
+
+        bBeginNotifications = (Button) findViewById(R.id.bBeginNotifications);
+
+        bBeginNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //createNotification(56, R.drawable.ic_action_name, "New Message", "There is a new message from Bob!");
+                createNotification2();
+            }
+        });
+
+
 
         setupServiceReceiver();
 
     }
+
+    private void createNotification(int nId, int iconRes, String title, String body) {
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("myChannelId", "My Channel", importance);
+            channel.setDescription("Reminders");
+            // Register the channel with the notifications manager
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.createNotificationChannel(channel);
+
+            NotificationCompat.Builder mBuilder =
+                    // Builder class for devices targeting API 26+ requires a channel ID
+                    new NotificationCompat.Builder(this, "myChannelId")
+                            .setSmallIcon(R.drawable.ic_action_name)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!");
+        } else {
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+                    this).setSmallIcon(iconRes)
+                    .setContentTitle(title)
+                    .setContentText(body);
+
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // mId allows you to update the notification later on.
+            mNotificationManager.notify(nId, mBuilder.build());
+        }
+    }
+
+    private void createNotification2() {
+        // First let's define the intent to trigger when notification is selected
+        // Start out by creating a normal intent (in this case to open an activity)
+        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent2 = new Intent(this, ViewPagerActvity.class);
+        // Next, let's turn this into a PendingIntent using
+        //   public static PendingIntent getActivity(Context context, int requestCode,
+        //       Intent intent, int flags)
+        int requestID = (int) System.currentTimeMillis(); //unique requestID to differentiate between various notification with same NotifId
+        int flags = PendingIntent.FLAG_CANCEL_CURRENT; // cancel old intent and create new one
+        PendingIntent pIntent = PendingIntent.getActivity(this, requestID, intent, flags);
+        PendingIntent pIntent2 = PendingIntent.getActivity(this, requestID, intent2, flags);
+        // Now we can attach the pendingIntent to a new notification using setContentIntent
+        Notification noti = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_action_name)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setContentIntent(pIntent)
+                .addAction(R.drawable.balon, "Go to View Pager", pIntent2)
+                .setAutoCancel(true) // Hides the notification after its been selected
+                .build();
+        // Get the notification manager system service
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(0, noti);
+    }
+
 
     /**
         The following 2 methods are for using an Intent Receiver and Communicating with the Main Activity
@@ -149,7 +228,7 @@ public class ServicesActivity extends AppCompatActivity {
     };
 
     /**
-        The following 2 classes are in conjunction with the PeriodicService and MyAlarmReceiver class
+        The following 2 methods are in conjunction with the PeriodicService and MyAlarmReceiver class
         Steps to Achieve a scheduler that triggers a background service at a regular interval
             1. Create the Intent Service
             2. Create the Broadcast Receiver
