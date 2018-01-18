16cd62ed73a9525d031cde0a1c9947b06d76bda4
Kevin Lay <kevinlay689@yahoo.com>
2018-01-17 22:50:42 -0800

    -Added TextView to show Kotlin class

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/kotlin/KotlinActivity.java
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/kotlin/KotlinActivity.java
@@ -3,6 +3,7 @@ package com.example.kevinlay.androidfundamentalspractice.kotlin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.kevinlay.androidfundamentalspractice.R;
 
@@ -10,11 +11,17 @@ public class KotlinActivity extends AppCompatActivity {
 
     private static final String TAG = "KotlinActivity";
 
+    private TextView tvKotlin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kotlin);
-        UserK userK = new UserK("test", 100);
+
+        tvKotlin = (TextView) findViewById(R.id.tvKotlin);
+
+        UserK userK = new UserK("This user was created using a Kotlin class", 100);
+        tvKotlin.setText(userK.showDetails());
 
         Log.i(TAG, "onCreate: "+ userK.showDetails());
         Log.i(TAG, "onCreate: "+ userK.p());
