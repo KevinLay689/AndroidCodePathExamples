ff8ff205acf6894504e718bb827fbc037fee26ae
Kevin Lay <kevinlay689@yahoo.com>
2018-01-18 21:23:14 -0800

    -Added Kotlin examples for null checks

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/kotlin/UserK.kt
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/kotlin/UserK.kt
@@ -89,5 +89,32 @@ class UserK(var title: String, var isbn: Long) : Foo() {
 
         }
     }
+
+    fun describeString(maybeString: String?): String {              // 1
+        if (maybeString != null && maybeString.length > 0) {        // 2
+            return "String of length ${maybeString.length}"
+        } else {
+            return "Empty or null string"
+        }
+    }
+    //1. A function that takes in a nullable String and gives you a phrase describing it
+    //2. If the given String is not null and not empty, tell the caller about it’s length
+
+    fun infoOnNull() {
+        var neverNull: String = "This can't be null"            // 1
+
+        var nullable: String? = "You can keep a null here"      // 2
+
+        nullable = null                                         // 3
+
+        var inferredNonNull = "The compiler assumes non-null"   // 4
+    }
+
+    //1. Declare a non-null String variable
+    //2. Declare a nullable String variable
+    //3. Set the nullable variable to null
+    //4. When infering types, the compiler assumes non-null for variables that are initialized with a value
 }
+
+
 
