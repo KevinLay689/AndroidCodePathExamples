455bd69305fc64cc645c8c466591a0f3790285dd
Kevin Lay <kevinlay689@yahoo.com>
2018-01-18 21:08:36 -0800

    -Added overrides for onMeasure() and onDraw()

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/kotlin/KotlinView.kt
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/kotlin/KotlinView.kt
@@ -1,6 +1,8 @@
 package com.example.kevinlay.androidfundamentalspractice.kotlin
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -9,9 +11,18 @@ import android.view.View
  */
 class KotlinView : View {
 
+    var textPaint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    var textPaint2 : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
 
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
 }
