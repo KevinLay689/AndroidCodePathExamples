8fe558f437dd0cc59c526f64c4b5361a97960d94
Kevin Lay <kevinlay689@yahoo.com>
2018-01-23 20:58:54 -0800

    -Added 3.2.2 Named arguments -Added notes for JVMOverloads

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/kotlin/kotlin_in_action/KotlinDefiningAndCallingFunctions.kt
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/kotlin/kotlin_in_action/KotlinDefiningAndCallingFunctions.kt
@@ -21,10 +21,47 @@ class KotlinDefiningAndCallingFunctions {
     fun printList() {
         println(list)
     }
-    // 3.2.1
+    // 3.2.1 Named arguments
     // The first problem we’ll address concerns the readability of function calls.
     // joinToString(collection, separator = " ", prefix = " ", postfix = ".")
     // can use strings in parameter to identify the parameters passed
     // When calling a method written in Kotlin, you can specify the names of some
     // arguments that you’re passing to the function.
+
+    // 3.2.2 Default parameter values
+    // In Kotlin, you can often avoid creating overloads because you can specify default
+    // values for parameters in a function declaration.
+    // You can either invoke the function with all the arguments or omit some of them because
+    // they have default values
+    // When using the regular call syntax, you can omit only trailing arguments. If you use
+    // named arguments, you can omit some arguments from the middle of the list and specify
+    // only the ones you need: joinToString(list, prefix = "# ")
+    fun <T> joinToString(
+            collection: Collection<T>,
+            separator: String = ", ",
+            prefix: String = "",
+            postfix: String = ""
+    ): String {
+        return "s"
+    }
+
+    /**
+        Given that Java doesn’t have the concept of default parameter
+        values, you have to specify all the parameter values explicitly when
+        you call a Kotlin function with default parameter values from Java. If
+        you frequently need to call a function from Java and want to make it
+        easier to use for Java callers, you can annotate it with
+        @JvmOverloads. This instructs the compiler to generate Java
+        overloaded functions, omitting each of the parameters one by one, starting from the last one.
+
+        For example, if you annotate joinToString() with
+        @JvmOverloads, the following overloads are generated:
+
+        /* Java */
+        String joinToString(Collection<T> collection, String separator,String prefix, String postfix);
+        String joinToString(Collection<T> collection, String separator,String prefix);
+        String joinToString(Collection<T> collection, String separator);
+        String joinToString(Collection<T> collection);
+     */
+
 }
