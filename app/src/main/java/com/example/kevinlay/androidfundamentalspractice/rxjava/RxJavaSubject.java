616a7f59404303899a2d16489850b8c2ce919df0
Kevin Lay <kevinlay689@yahoo.com>
2018-01-27 18:58:58 -0800

    -Added Lambda support

--- app/src/main/java/com/example/kevinlay/androidfundamentalspractice/rxjava/RxJavaSubject.java
+++ app/src/main/java/com/example/kevinlay/androidfundamentalspractice/rxjava/RxJavaSubject.java
@@ -1,5 +1,7 @@
 package com.example.kevinlay.androidfundamentalspractice.rxjava;
 
+import android.util.Log;
+
 import rx.subjects.PublishSubject;
 
 /**
@@ -18,11 +20,19 @@ public class RxJavaSubject {
     public void createPublishSubject() {
         // As we can see in the example, 1 isn't printed because we weren't subscribed when it was pushed.
         // After we subscribed, we began receiving the values that were pushed to the subject.
+        // This is the first time we see subscribe being used, so it is worth paying attention to how it was used.
+        // In this case, we used the overload which expects one Function for the case of onNext. That function
+        // takes an argument of type Integer and returns nothing. Functions without a return type are also
+        // called actions. We can provide that function in different ways:
         PublishSubject<Integer> subject = PublishSubject.create();
         subject.onNext(1);
-        subject.subscribe();
+        subject.subscribe((v) -> handleInt(v));
         subject.onNext(2);
         subject.onNext(3);
         subject.onNext(4);
+    }
+
+    void handleInt(int v) {
+        Log.i("", "handleInt: "+ v);
     }
 }
